9e846e79cab265fb03ecc8ac9c86fa08823c0f49
taiwbi <taiwbii@proton.me>
2026-08-05 23:16:41 +0330

    feat(ui): use minimal Noice notifications and messages
    Configure notifications, messages, and LSP messages to use the
    compact mini view instead of larger displays.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,7 +2,20 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
+    notify = {
+      enabled = true,
+      view = "mini",
+    },
+    messages = {
+      view = "mini",
+      view_error = "mini",
+      view_warn = "mini",
+    },
     lsp = {
+      message = {
+        enabled = true,
+        view = "mini",
+      },
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
